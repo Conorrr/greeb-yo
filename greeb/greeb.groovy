53893314b22ed6c3b27283ced180a88e5cb28c6b
Conor Restall <Conorrr@users.noreply.github.com>
2016-07-16 15:06:56 +0100

    basically a rewrite

--- greeb/greeb.groovy
+++ greeb/greeb.groovy
@@ -9,52 +9,46 @@ greeb {
   credentials new File('discord.token').text
   properties("properties.json")
 
-  String mainChannelName = properties.mainChannelName
-  String welcomeChannelName = properties.welcomeChannelName
+  String mainChannelId = properties.mainChannelId
   def regions = properties.regions
 
   List<IRole> roles
   IGuild guild
   IChannel mainChannel
-  IChannel welcomeChannel
 
   consumers {
+
+    // HELPER METHODS  //
+    def regionBullets = regions.inject("") { result, region -> result + "\n• !$region" }
+    /////////////////////
+
+    messageReceived(/^!ping/) {
+      respond("pong")
+    }
 
     guildCreate(all()) {
       // currently only support for 1 guild
       guild = event.guild
       roles = guild.roles
-      mainChannel = guild.channels.find { it.name == mainChannelName }
-      welcomeChannel = guild.channels.find { it.name == welcomeChannelName }
+      mainChannel = guild.getChannelByID(mainChannelId)
     }
 
     userJoin(all()) {
-      mainChannel.sendMessage("""\
-        Hey <@!$user.ID>
-        Welcome to Keep Calm.
+      List<IRole> currentRoles = user.getRolesForGuild(guild)
+      def newRole = guild.getRoleByID(properties.starterRole)
 
-        Please check the <#$welcomeChannel.ID> page first of all. !NA, !EUW, !EUNE, !LAN, !LAS, !BR or !OCR and the bot will assign you a region tag.
+      guild.editUserRoles(user, (currentRoles + newRole) as IRole[])
+
+      mainChannel.sendMessage("""\
+        Welcome to Calm Yo' <@!$user.ID>  - Feel free to make games, and get to know everyone. If you need any help, give Yo' Team a shout!
+
         GL & HF 😃""".stripIndent())
     }
 
-    regions.each { region ->
-      privateMessageReceived(/^!$region/) chain messageReceived(/^!$region/, mainChannelName) {
-        List<IRole> currentRoles = user.getRolesForGuild(guild)
+    messageReceived(/(?i)^!regions/) {
+      def message = "Here is a list of region tags you can choose from, choose wisely! \nPlease reply to this message with:"
 
-        def alreadyAssigned = currentRoles.find { regions.contains(it.name) }
-
-        if (!alreadyAssigned) {
-          def newRole = guild.getRoles().find { it.name == region }
-          guild.editUserRoles(user, (currentRoles + newRole) as IRole[])
-          respond("<@!${user.ID}> you are now assigned to `$region`")
-        } else {
-          respond("<@!$user.ID> you are already assigned to $alreadyAssigned.name. If you wish to change region please use `!resetregion` if you wish to change your region use !NA, !EUW, !EUNE, !LAN, !LAS, !BR or !OCR.")
-        }
-      }
-    }
-
-    messageReceived(/^!ping/) {
-      respond("pong")
+      client.getOrCreatePMChannel(user).sendMessage(message + regionBullets)
     }
 
     messageReceived(/^!resetregion/) {
@@ -62,11 +56,31 @@ greeb {
 
       def currentRegion = currentRoles.find { regions.contains(it.name) }
 
+      def message
+
       if (currentRegion) {
         guild.editUserRoles(user, (currentRoles - currentRegion) as IRole[])
-        respond("<@!$user.ID> you are now unassigned to $currentRegion.name. If you wish to set a region use !NA, !EUW, !EUNE, !LAN, !LAS, !BR or !OCR.")
+        message = "Your region tag has been reset, To join a new region reply to this message with a region tag from the list below."
       } else {
-        respond("<@!$user.ID> you aren't assigned to any regions. If you wish to set a region use !NA, !EUW, !EUNE, !LAN, !LAS, !BR or !OCR.")
+        message = "You aren't assigned to any regions. To join a region reply to this message with a region tag from the list below."
+      }
+
+      client.getOrCreatePMChannel(user).sendMessage(message + regionBullets)
+    }
+
+    regions.each { region ->
+      messageReceived(/^!$region/) {
+        List<IRole> currentRoles = user.getRolesForGuild(guild)
+
+        def alreadyAssigned = currentRoles.find { regions.contains(it.name) }
+
+        if (!alreadyAssigned) {
+          def newRole = guild.getRoles().find { it.name == region }
+          guild.editUserRoles(user, (currentRoles + newRole) as IRole[])
+          client.getOrCreatePMChannel(user).sendMessage("You are now assigned to `$region`")
+        } else {
+          client.getOrCreatePMChannel(user).sendMessage("You are already assigned to $alreadyAssigned.name. If you wish to change region please use `!resetregion` if you wish to change your region use $regionBullets.")
+        }
       }
     }
 
